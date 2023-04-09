1ed8771af1ef16a17b2362a25873fce802077ecc
Ishan Bajpai <82746770+ishancrat@users.noreply.github.com>
2023-04-09 16:34:45 +0530

    Update Lab5.m

--- Lab5.m
+++ Lab5.m
@@ -1,7 +1,18 @@
 % cconv w/o 'n' computes linear convolution
 % cconv computes properly when provided with some 'n' value
 
-x=[1,2,-1,-2,3,1]
-h=[3,2,1]
+t=-5:0.5:5;
+x=sin(t)
+h=[3,2,1,6,7,4,6,7,8]
 
-cconv(x,h,6)
+subplot(3,1,1)
+stem(x,'Linewidth',1)
+title('First Signal','fontweight','bold')
+subplot(3,1,2)
+stem(h,'Linewidth',1)
+title('Second Signal','fontweight','bold')
+
+y=cconv(x,h,length(t));
+subplot(3,1,3)
+stem(y,'Linewidth',1)
+title('Circularly Convolved Signal','fontweight','bold')
